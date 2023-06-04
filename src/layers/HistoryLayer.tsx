3a9a94918bec362d0718b90e601feeb63d2bdf63
requinDr <75610214+requinDr@users.noreply.github.com>
2023-06-04 18:29:20 +0200

    fix incorrect display last page history

--- src/layers/HistoryLayer.tsx
+++ src/layers/HistoryLayer.tsx
@@ -73,13 +73,13 @@ const HistoryLayer = ({ pages, text }: Props) => {
           {/* lignes des pages précédentes */}
           {pages.map((page, i) =>
             page.map((line: any, j: any) =>
-              <LineComponent key={`${i}_${j}`} line={line} printInstantly={true} />
+              <LineComponent key={`histo_page_${i}_${j}`} line={line} printInstantly={true} />
             )
           )}
 
           {/* lignes de la page actuelle */}
           {text.map((line, i) =>
-            <LineComponent key={i} line={line} printInstantly={true} />
+            <LineComponent key={`histo_text_${i}`} line={line} printInstantly={true} />
           )}
         </div>
       </div>
--- src/layers/TextLayer.tsx
+++ src/layers/TextLayer.tsx
@@ -25,9 +25,11 @@ const TextLayer = ({ text, handleClick }: Props) => {
   return (
     <div className={`box box-text ${state.dispText ? "" : "hide"}`} onClick={handleClick}>
       <div className="text-container">
-        {/* should print instantly only if it's not the last line of text */}
         {text.map((line, i) =>
-          <LineComponent key={i} line={line} printInstantly={i === text.length - 1 ? false : true} isLastLine={text.length - 1 === i} />
+          <LineComponent key={`text_${i}`}
+            line={line}
+            printInstantly={i === text.length - 1 ? false : true}
+            isLastLine={text.length - 1 === i} />
         )}
       </div>
     </div>
