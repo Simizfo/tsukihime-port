aef2e1ce0fdd0b6e9bcebf79509c9fbeef2a1d70
loicfrance <loicfrance.developer@gmail.com>
2023-06-10 22:59:26 +0200

    Add lshutter transition support

--- src/layers/BackgroundLayer.tsx
+++ src/layers/BackgroundLayer.tsx
@@ -13,21 +13,24 @@ const BackgroundLayer = ({ bg }: Props) => {
   const bgTmp = useRef<Background>(bg);
   const extension = state.permanent.imagesFolder === 'image' ? 'jpg' : 'webp';
 
+  let effect = '',
+      duration = 0
+
   useEffect(() => {
-    if (bg.type === BgTransition.type_crossfade_slw) {
-      crossfade()
+    if (duration > 0) {
+      crossfade(duration)
     } else {
       bgTmp.current = bg;
     }
   }, [bg]);
 
-  const crossfade = () => {
+  const crossfade = (duration: number) => {
     dispatch({ type: 'SET_DISP_TEXT', payload: false })
     setFade(true);
     setTimeout(() => {
       bgTmp.current = bg;
       setFade(false);
-    }, 1000);
+    }, duration);
   }
 
   useEffect(() => {
@@ -36,25 +39,65 @@ const BackgroundLayer = ({ bg }: Props) => {
     }
   }, [fade, dispatch]);
 
-  if (bg.image.startsWith('#')) {
-    return (
-      <div className="background" style={{ background: bg.image }} />
-    );
-  } else {
+  function getTransition(type: string) {
+    let duration = 0
+    let effect = type
+    
+    if (effect.startsWith('type_'))
+      effect = effect.substring('type_'.length)
+    
+    const index = effect.lastIndexOf('_')
+    if (index !== -1) {
+      let speed = effect.substring(index+1);
+      switch(speed) {
+        case 'slw': duration = 1500; break
+        case 'mid': duration = 800; break
+        case 'fst': duration = 400; break
+        default : throw Error(`Ill-formed effect '${effect}'`);
+      }
+      effect = effect.substring(0, index);
+    }
+    return {effect, duration}
+  }
+
+  const createImg = (bg: Background, transition: object = {})=> {
+    const {image, type} = bg;
+    const attrs = {
+      className: "background",
+      ...transition
+    }
+    if (image.startsWith('#')) {
+      return (
+        <div
+          style={{ background: image }}
+          {...attrs}
+        />
+      )
+    }
+    else {
+      return (
+        <img
+          src={`${state.permanent.imagesFolder}/${image}.${extension}`}
+          alt="background"
+          {...attrs}
+        />
+      )
+    }
+  }
+
+  ({effect, duration} = getTransition(bg.type));
+
+  if (bgTmp.current != bg && duration > 0) {
+    const elmt1 = createImg(bg),
+          elmt2 = createImg(bgTmp.current,{style:{'--transition-time': `${duration}ms`}, 'fade-effect': effect})
     return (
       <>
-        <img
-          src={`${state.permanent.imagesFolder}/${bg.image}.${extension}`}
-          alt="background"
-          className={`background`}
-        />
-        <img
-          src={`${state.permanent.imagesFolder}/${bgTmp.current.image}.${extension}`}
-          alt="background"
-          className={`background ${fade ? 'fade' : ''}`}
-        />
+        {elmt1}
+        {elmt2}
       </>
-    );
+    )
+  } else {
+    return createImg(bg)
   }
 };
 
