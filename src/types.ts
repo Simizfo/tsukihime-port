ee5b2f29dea95d1170249bc0d7f0db0ae2c6ad9c
loicfrance <loicfrance.developer@gmail.com>
2023-07-14 19:41:31 +0200

    clean indentation in types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,29 @@
 import { IMAGES_FOLDERS, TEXT_SPEED } from "./utils/constants"
 
 export type Line = {
-    line: string,
-    lineHasEnded?: boolean,
-    read?: boolean,
+  line: string,
+  lineHasEnded?: boolean,
+  read?: boolean,
 }
 
 export type Page = Line[]
 
 export type Choice = {
-    libe: string,
-    f: number,
+  libe: string,
+  f: number,
 }
 
 //ld c,":a;image\tachi\stk_t01.jpg",%type_lshutter_fst
 export type Character = {
-    image: string,
-    type: string,
-    pos: string, //r, l, c
+  image: string,
+  type: string,
+  pos: string, //r, l, c
 }
 
 //bg "image\bg\bg_05a.jpg",%type_lcartain_mid
 export type Background = {
-    image: string,
-    type: string,
+  image: string,
+  type: string,
 }
 
 export type ContextState = {
