fa709d76067d104162cbe1464f2e17ede46e7664
Loic France <loicfrance.developer@gmail.com>
2023-07-18 15:53:29 +0200

    export sNa scenes, openning and ending, parse KT

--- script-convert/script-convert.js
+++ script-convert/script-convert.js
@@ -1,112 +1,114 @@
 const fs = require('fs');
 
-let script = fs.readFileSync('full-script.txt', 'utf8');
-
 //if not exist create a folder "scenes"
-function createScenesFolder() {
-    if (!fs.existsSync('scenes')) {
-        fs.mkdirSync('scenes');
+function createDir(dirName) {
+    if (!fs.existsSync(dirName)) {
+        fs.mkdirSync(dirName);
     }
 }
-createScenesFolder();
 
 //script that take edited-script.txt and remove all lines that start with ;
-function removeComments(script) {
-    const lines = script.split(/\r?\n/);
-
-    const regex = /^;/;
-
-    let newLines = [];
-
-    lines.forEach((line) => {
-        if (!regex.test(line)) {
-            newLines.push(line);
-        }
-    });
-
-    return newLines.join('\n');
+function filterLines(scriptLines, regexp) {
+    return scriptLines.filter(line=>regexp.test(line))
 }
 
-script = removeComments(script);
-
 //take all numalias flgJ,118 and create a json file with the key being the number and the value being the alias
-function createNumAliasJson(script) {
-    const lines = script.split(/\r?\n/);
+function extractNumAliasJson(scriptLines, fileName) {
 
     const regex = /^numalias (\w+),(\d+)/;
 
     let numAlias = {};
 
-    lines.forEach((line) => {
+    const remainingLines = []
+    for(const line of scriptLines) {
         if (regex.test(line)) {
             let alias = line.match(regex)[1];
             let num = line.match(regex)[2];
             numAlias[num] = alias;
+        } else {
+            remainingLines.push(line)
         }
-    });
+    }
 
-    fs.writeFileSync('numalias.json', JSON.stringify(numAlias));
+    fs.writeFileSync(fileName, JSON.stringify(numAlias));
+    return remainingLines
 }
-createNumAliasJson(script);
 
 //take all stralias se6,"wave\se_07.wav" and create a json file with the key being the number and the value being the alias
-function createStrAliasJson(script) {
-    const lines = script.split(/\r?\n/);
+function extractStrAliasJson(scriptLines, fileName) {
 
     const regex = /^stralias (\w+),"(.+)"/;
 
     let strAlias = {};
 
-    lines.forEach((line) => {
+    const remainingLines = []
+    for(const line of scriptLines) {
         if (regex.test(line)) {
             let alias = line.match(regex)[1];
             let str = line.match(regex)[2];
             strAlias[alias] = str;
+        } else {
+            remainingLines.push(line)
         }
-    });
+    }
 
-    fs.writeFileSync('stralias.json', JSON.stringify(strAlias));
+    fs.writeFileSync(fileName, JSON.stringify(strAlias));
+    return remainingLines
 }
-createStrAliasJson(script);
 
 //replace each | by a …
-function replacePipeByEllipsis(script) {
-    const lines = script.split(/\r?\n/);
-
-    const search = '|';
-    const replacement = '…';
-
-    let newLines = [];
-
-    lines.forEach((line) => {
-        if (line.includes(search)) {
-            newLines.push(line.replaceAll(search, replacement));
-        } else {
-            newLines.push(line);
+function replacePipeByEllipsis(scriptLines) {
+    for (let [i, line] of scriptLines.entries()) {
+        if (line.startsWith('`') && line.includes('|')) {
+            line = line.replaceAll('|', '…')
+            scriptLines[i] = line
         }
-    });
-
-    return newLines.join('\n');
+    }
+    return scriptLines
 }
-script = replacePipeByEllipsis(script);
 
 //write a txt file in folder "scenes" for each scene
 //a scene is defined by a line that starts with *s and is followed by a number
-function writeScenes(script) {
-    const lines = script.split(/\r?\n/);
-    const regex = /^\*s(\d+)/;
+function writeScenes(scriptLines, dir) {
+    const regex = /^\*(s(?<scene>\d+a?)|(?<op>openn?ing)|(?<ed>ending))/;
     let scene = [];
-    let sceneNumber = 0;
-    lines.forEach((line) => {
+    let sceneId = '0';
+    scriptLines.forEach((line) => {
         if (regex.test(line)) {
-            fs.writeFileSync(`scenes/scene${sceneNumber}.txt`, scene.join('\n'));
+            //console.log(sceneId, scene.length)
+            fs.writeFileSync(`${dir}/scene${sceneId}.txt`, scene.join('\n'));
             scene = [];
-            // Extract the scene number
-            sceneNumber = parseInt(line.match(regex)[1], 10);
+            // Extract the next scene id
+            const groups = line.match(regex).groups
+            sceneId = groups['scene'] ?? groups['op'] ?? groups['ed'];
         } else {
             scene.push(line);
         }
     });
 }
 
-writeScenes(script);
+function main() {
+    const scripts = {
+        'full-script.txt': "",
+        'full-script-kt.txt': "-kt"
+    }
+    for (const [file, suffix] of Object.entries(scripts)) {
+        const dir = `scenes${suffix}`
+        createDir(dir)
+        let script = fs.readFileSync(file, 'utf8');
+        let lines = script.split(/\r?\n/);
+        console.log(lines.length)
+        lines = filterLines(lines, /^(?!;)/); // remove comments
+        console.log(lines.length)
+        lines = filterLines(lines, /^(?!numalias )/); // remove num aliases
+        console.log(lines.length)
+        lines = filterLines(lines, /^(?!effect )/); // remove effects aliases
+        console.log(lines.length)
+        lines = extractStrAliasJson(lines, `stralias${suffix}.json`)
+        console.log(lines.length)
+        lines = replacePipeByEllipsis(lines)
+        writeScenes(lines, dir)
+    }
+}
+
+main()
