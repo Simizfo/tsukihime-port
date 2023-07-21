1f9b0dd2629a53b587d4f9469db28c284842bd9a
requinDr <75610214+requinDr@users.noreply.github.com>
2023-07-21 14:40:02 +0200

    remove Electron
    Starts dev with `npm start`

--- README.md
+++ README.md
@@ -3,10 +3,9 @@ Web port of Tsukihime
 
 ## Installation
 1) Install dependencies with `npm i` (Vite requires node `14.18+`)
-2) Launch with `npm run dev`
+2) Launch with `npm start`
 
 ## Frameworks & languages
 - React (Vite)
-- Electron
 - TypeScript
 - SASS
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,12 @@
-import { defineConfig } from 'vite'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    electron([
-      {
-        // Main-Process entry file of the Electron App.
-        entry: 'electron/main.ts',
-      },
-      {
-        entry: 'electron/preload.ts',
-        onstart(options) {
-          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
-          // instead of restarting the entire Electron App.
-          options.reload()
-        },
-      },
-    ]),
-    renderer(),
-  ],
-})
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  define: {
+    'process.env': {}
+  }
+});
