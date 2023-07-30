20f30c1cc880fbd31dc743b46e55fabd86a5d8fd
requinDr <75610214+requinDr@users.noreply.github.com>
2023-07-30 17:53:31 +0200

    H scene warning added to config

--- src/screens/config/ConfigAdultTab.tsx
+++ src/screens/config/ConfigAdultTab.tsx
@@ -4,11 +4,13 @@ import { settings } from "../../utils/variables"
 
 const defaultConf = {
   blurThumbnails: true,
+  warnHScenes: false
 }
 
 const ConfigAdultTab = () => {
   const [conf, setConf] = useState({
     blurThumbnails: settings.blurThumbnails,
+    warnHScenes: settings.warnHScenes,
   })
 
   useEffect(()=> {
@@ -29,6 +31,17 @@ const ConfigAdultTab = () => {
           { text: 'Off', value: false },
         ]}
         property="blurThumbnails"
+        conf={conf}
+        updateValue={updateValue}
+      />
+
+      <ConfigButtons
+        title="Warn me when I enter an H scene"
+        btns={[
+          { text: 'On', value: true },
+          { text: 'Off', value: false },
+        ]}
+        property="warnHScenes"
         conf={conf}
         updateValue={updateValue}
       />
