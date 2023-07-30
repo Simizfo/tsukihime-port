ff66254fd4fcc85d904836541259ad5ed6081458
requinDr <75610214+requinDr@users.noreply.github.com>
2023-07-30 18:16:15 +0200

    Fullscreen option added to config

--- src/screens/config/ConfigAdvancedTab.tsx
+++ src/screens/config/ConfigAdvancedTab.tsx
@@ -11,6 +11,9 @@ const ConfigAdvancedTab = () => {
   const [conf, setConf] = useState({
     imagesFolder: settings.imagesFolder,
   })
+  const [fullscreen, setFullscreen] = useState<boolean>(
+    document.fullscreenElement !== null
+  ) // don't save in settings
 
   useEffect(()=> {
     Object.assign(settings, conf)
@@ -20,6 +23,11 @@ const ConfigAdvancedTab = () => {
     key: T,
     value: typeof defaultConf[T]
   ) => setConf(prev => ({ ...prev, [key]: value }))
+
+  const toggleFullscreen = () => {
+    fullscreen ? document.exitFullscreen() : document.documentElement.requestFullscreen()
+    setFullscreen(prev=> !prev)
+  }
 
   return (
     <section>
@@ -32,6 +40,17 @@ const ConfigAdvancedTab = () => {
         property="imagesFolder"
         conf={conf}
         updateValue={updateValue}
+      />
+
+      <ConfigButtons
+        title="Fullscreen"
+        btns={[
+          { text: `On`, value: true },
+          { text: `Off`, value: false },
+        ]}
+        property="fullscreen"
+        conf={{fullscreen}}
+        updateValue={toggleFullscreen}
       />
 
       <ResetBtn onClick={() => setConf(defaultConf)} />
