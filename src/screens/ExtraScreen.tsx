61c591c626443e3365e468a9738a76bab5e7b5b5
requinDr <75610214+requinDr@users.noreply.github.com>
2023-08-15 10:41:19 +0200

    Extra screen initial style

--- src/screens/ExtraScreen.tsx
+++ src/screens/ExtraScreen.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/extra.scss'
 import { motion } from 'framer-motion'
-import strings from '../utils/lang'
+import strings, { imageUrl } from '../utils/lang'
 import { SCREEN } from '../utils/display'
+import { settings } from '../utils/variables'
 
 
 const ExtraScreen = () => {
-
+  console.log(settings.eventImages)
   return (
     <motion.div
       className="page" id="extra"
@@ -17,9 +17,27 @@ const ExtraScreen = () => {
       <div className="page-content">
         <h2 className="page-title">Extra</h2>
         <main>
+
+          <section id="extra-gallery">
+            <div className='gallery-previews'>
+              {/* todo: filter out sensitive images (don't even blur them) */}
+              {/* display 8 random images */}
+              {settings.eventImages.sort(() => Math.random() - 0.5).slice(0, 8).map((image, index) =>
+                <img key={index} src={imageUrl(image)} alt="event" />
+              )}
+
+              {/* Placeholders */}
+              {Array(Math.max(0, 8 - settings.eventImages.length)).fill(0).map((_, index) =>
+                <img key={index} src={imageUrl("notreg")} alt="placeholder" />
+              )}
+            </div>
+
+            <Link to={SCREEN.GALLERY} className="page-link">Gallery</Link>
+          </section>
           
-          <Link to={SCREEN.GALLERY}>Gallery</Link>
-          <Link to={SCREEN.ENDINGS}>Endings</Link>
+          <section>
+            <Link to={SCREEN.ENDINGS} className="page-link">Endings</Link>
+          </section>
         </main>
 
         <Link to={SCREEN.TITLE} className="menu-btn back-button">{strings.back}</Link>
