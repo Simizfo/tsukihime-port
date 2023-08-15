c3b7d86b9ac7b675c5ce454b8e7da30d47dd0866
loicfrance <loicfrance.developer@gmail.com>
2023-08-15 16:11:18 +0200

    Update disclaimer when translation is loaded

--- src/screens/DisclaimerScreen.tsx
+++ src/screens/DisclaimerScreen.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
 import '../styles/title-menu.scss'
 import { motion } from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
 import strings from '../utils/lang'
 import { bb } from '../utils/utils'
+import { useObserver } from '../utils/Observer'
 
 const DisclaimerScreen = () => {
   const navigate = useNavigate()
+  const [_updateNum, forceUpdate] = useReducer(x => (x + 1) % 100, 0);
+  useObserver(forceUpdate, strings, 'translation-name')
 
   useEffect(()=> {
     const timeout = setTimeout(()=> {
