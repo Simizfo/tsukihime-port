89e491a7ae82f34297b0caa3afa056c0514fb605
requinDr <75610214+requinDr@users.noreply.github.com>
2023-09-13 21:11:55 +0200

    Remove save unfocus onmouseleave

--- src/components/SavesLayout.tsx
+++ src/components/SavesLayout.tsx
@@ -1,15 +1,14 @@
-import { ChangeEvent, MouseEvent, Fragment, memo, useEffect, useState } from "react"
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react"
 import { SCREEN, displayMode } from "../utils/display"
 import { bb, wbb } from "../utils/Bbcode"
-import { PageContent, SceneName } from "../types"
+import { SceneName } from "../types"
 import { SAVE_EXT } from "../utils/constants"
 import { SaveState, QUICK_SAVE_ID, deleteSaveState, getSaveState, listSaveStates, loadSaveState, storeCurrentState, addSavesChangeListener, removeSavesChangeListener, exportSave, loadSaveFiles } from "../utils/savestates"
 import { getSceneTitle } from "../utils/scriptUtils"
-import { BsDownload, BsFileEarmarkArrowUp, BsPlusCircle, BsTrash } from "react-icons/bs"
+import { BsFileEarmarkArrowUp, BsPlusCircle } from "react-icons/bs"
 import strings from "../utils/lang"
-import { Tooltip } from 'react-tooltip'
-import { settings } from "../utils/variables"
-import { graphicElements } from "./GraphicsComponent"
+import SaveListItem from "./save/SaveListItem"
+import SaveDetails from "./save/SaveDetails"
 
 //##############################################################################
 //#                               TOOL FUNCTIONS                               #
@@ -22,7 +21,7 @@ function compareSaveStates([id1, ss1]: [number, SaveState], [id2, ss2]: [number,
       : (ss2.date ?? 0) - (ss1.date ?? 0)
 }
 
-function phaseTitle(saveState: SaveState) {
+export function phaseTitle(saveState: SaveState) {
   const context = saveState.context
   const phase = context.phase
   if (!phase || !phase.route || !phase.routeDay) {
@@ -31,110 +30,19 @@ function phaseTitle(saveState: SaveState) {
   return wbb(strings.scenario.routes[phase.route][phase.routeDay])
 }
 
-function phaseDay(saveState: SaveState) {
+export function phaseDay(saveState: SaveState) {
   const day = saveState.context.phase?.day
   return day ? wbb(strings.scenario.days[day-1]) : ""
 }
 
-//##############################################################################
-//#                               SUB COMPONENTS                               #
-//##############################################################################
-
-const SaveSummary = memo(({saveState}: {saveState: SaveState})=> {
-  const page = saveState.page
-  switch (page?.contentType) {
-    case "text" :
-      return <>{(page as PageContent<"text">).text.trim()}</>
-    case "choice" :
-      const {choices, selected: sel} = (page as PageContent<"choice">)
-      return <>{choices.map(({index: i, str})=>
-        <Fragment key={i}>{i > 0 && <>, </>}
-          <span className={`choice ${sel == i ? "selected" : ""}`} key={i}>
-            {str}
-          </span>
-        </Fragment>
-      )}</>
-    case "skip" :
-      return <span className="scene-skip">
-        {getSceneTitle((page as PageContent<"skip">).scene)}
-      </span>
-    case "phase" :
-      const day = phaseDay(saveState)
-      return <>
-        {phaseTitle(saveState)}
-        {day && <>, {day}</>}
-      </>
-    default :
-      throw Error(`unimplemented page type`)
-  }
-})
-
-type SaveListItemProps = {
-  id: number,
-  saveState: SaveState,
-  onSelect: (id: number)=>void,
-  focusedSave?: number,
-  [key: string]: any
-}
-const SaveListItem = ({id, saveState, onSelect, focusedSave, ...props}: SaveListItemProps)=> {
-  const date = new Date(saveState.date as number)
-  return (
-    <button className={`save-container ${id==focusedSave ? "active" : ""}`}
-        onClick={onSelect.bind(null, id)}
-        {...(id==QUICK_SAVE_ID ? {'quick-save':''} : {})}
-        {...props}>
-      <div className={`graphics ${saveState.context.monochrome ? "monochrome" : ""}`}>
-        {graphicElements(saveState.graphics ?? saveState.context.graphics ?? {bg: ""}, {}, 'sd')}
-      </div>
-      <div className="deta">
-        <div className="date">
-          <b>{date.toLocaleDateString(strings.locale)}</b> {date.toLocaleTimeString(strings.locale)}
-        </div>
-        <div className="line">
-          <SaveSummary saveState={saveState}/>
-        </div>
-      </div>
-    </button>
-  )
-}
-type SaveDetailsProps = {
-  id?: number, saveState?: SaveState, deleteSave: (id: number)=>void,
-  [key:string]: any
-}
-const SaveDetails = ({id, saveState, deleteSave, ...props}: SaveDetailsProps)=> {
-  return (
-    <div className="info" {...props}>
-      <div className="graphics">
-        {graphicElements(saveState?.graphics ?? saveState?.context.graphics ?? {bg:"notreg"}, {}, settings.resolution)}
-      </div>
-      {id != undefined && saveState != undefined &&
-        <div className="deta">
-          <div>{phaseTitle(saveState)}</div>
-          <div>{phaseDay(saveState)}</div>
-
-          <div className="actions">
-            <Tooltip id="tooltip" className="tooltip" delayShow={800} />
-            <button onClick={deleteSave.bind(null, id)}
-              data-tooltip-id="tooltip" data-tooltip-content="Delete">
-              <BsTrash />
-            </button>
-            <button onClick={()=>exportSave([id])}
-              data-tooltip-id="tooltip" data-tooltip-content="Download">
-              <BsDownload />
-            </button>
-          </div>
-        </div>
-      }
-    </div>
-  )
-}
 
 //##############################################################################
 //#                               MAIN COMPONENT                               #
 //##############################################################################
+
 type Props = {
   variant: "save"|"load",
-  back: (saveLoaded:boolean)=>void,
+  back: (saveLoaded: boolean)=>void,
 }
 const SavesLayer = ({variant, back}: Props) => {
   const [saves, setSaves] = useState<Array<[number,SaveState]>>([])
@@ -184,8 +92,8 @@ const SavesLayer = ({variant, back}: Props) => {
   }
 
   const focusedSave = focusedId != undefined ? getSaveState(focusedId) : undefined
-  console.log(focusedId)
   const title = strings.saves[variant == "save" ? "title-save" : "title-load"]
+
   return (
     <main id="saves-layout">
       <h2 className="page-title">{title}</h2>
@@ -209,7 +117,6 @@ const SavesLayer = ({variant, back}: Props) => {
             onFocus={setFocusedSave.bind(null, 2)}
             onPointerEnter={setFocusedSave.bind(null, 2)}
             onMouseEnter={setFocusedSave.bind(null, 2)}
-            onMouseLeave={setFocusedSave.bind(null, undefined)}
           >
             <BsFileEarmarkArrowUp />
           </label>
@@ -226,7 +133,6 @@ const SavesLayer = ({variant, back}: Props) => {
             onFocus={setFocusedSave.bind(null, id)}
             onPointerEnter={setFocusedSave.bind(null, id)}
             onMouseEnter={setFocusedSave.bind(null, id)}
-            onMouseLeave={setFocusedSave.bind(null, undefined)}
           />
         )}
       </div>
